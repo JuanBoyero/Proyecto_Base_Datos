26ec9a37d9bc0c91ee7809052665ad68fcd80d77
Juan Boyero <@juaniboyerooo@gmail.com>
2025-10-29 10:10:13 -0300

    Cambio estructura del menu y corrijo implementacion vacia de metodos

---
+++ proyectoBD/src/main/java/Sistema.java
@@ -0,0 +1,29 @@
+
+
+public class Sistema {
+	public Sistema(){
+	}
+
+	public void pantallaPrincipal(){
+		System.out.println(
+							"---Menu de opciones---\n" +
+							"[1] Insertar un usuario\n" +
+							"[2] Eliminar un reclamo\n" +
+							"[3] Listar los reclamos\n" +
+							"Digite cualquier otro numero para salir del sistema \n"
+							);
+	}
+
+	public void ingresarUsuario(){
+		throw new UnsupportedOperationException("Metodo no implementado peleles");	
+	}
+
+	public void eliminarReclamo(){
+		throw new UnsupportedOperationException("Metodo no implementado peleles");	
+	}
+
+	public String listarReclamos(){
+		throw new UnsupportedOperationException("Metodo no implementado peleles");	
+	}
+	
+}
